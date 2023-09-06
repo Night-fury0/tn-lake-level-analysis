a83766652b0fd8c21b29148571a5d5c331e843c0
sriram <sriram19990127@gmail.com>
2023-09-06 23:10:56 +0530

    pyspark code to fetch data of chemb in given date range

--- main.py
+++ main.py
@@ -3,26 +3,43 @@ from bs4 import BeautifulSoup
 from io import StringIO
 from pyspark.sql import SparkSession
 import requests
+import datetime
 
-date = "2023-09-05"
-url = f"https://cmwssb.tn.gov.in/lake-level?date={date}"
+def date_range(start_date, end_date):
+	for n in range(int((end_date-start_date).days)):
+		yield start_date + datetime.timedelta(n)
 
-request=  requests.get(url)
 
-soup = BeautifulSoup(request.text, 'html.parser')
+#date = "2023-09-05"
+year = 2022
+start_date = datetime.date(year, 1,1)
+end_date = datetime.date(year,1,10)
 
-table = soup.find_all("table")[-1]
+dates = [[str(i)] for i in date_range(start_date, end_date)]
 
-html_stream = StringIO(str(table))
-df = pd.read_html(html_stream)[0]
 
-#print(df)
+def getStorage(date):
+	url = f"https://cmwssb.tn.gov.in/lake-level?date={date}"
+	request=  requests.get(url)
+	soup = BeautifulSoup(request.text, 'html.parser')
+	table = soup.find_all("table")[-1]
+	values = table.find_all("td")
+	#html_stream = StringIO(str(table))
+	#df = pd.read_html(htmli_stream)[0]
+	return tuple([date, values[44].text, values[45].text,values[46].text, values[47].text,values[48].text])
+	#print(df)
 
+columns = ['date','storage','storage_pct','inflow','outflow','rainfall']
 spark = SparkSession.builder.appName('demo').getOrCreate()
-lake = spark.createDataFrame(df)
-lake.show()
+df = spark.createDataFrame(dates, ["date"])
+df = df.rdd.map( lambda row : getStorage(row[0]) ).toDF(columns)
 
+df.write.csv("results")
 
+#lake = spark.createDataFrame(df)
+#lake.show()
+
+#print(dir(lake))
 
 
 #emptyRDD = spark.sparkContext.emptyRDD()
