96a472773eda0efae2836f32b2b953898a60c57e
sriram <sriram19990127@gmail.com>
2023-09-08 10:38:40 +0530

    updated query to show date of min/max

--- read_data.py
+++ read_data.py
@@ -1,22 +1,25 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import year,month,round, col
+from pyspark.sql.functions import year,month,round,col
 
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.csv("data/*/", inferSchema=True, header=True)
 
 df = df.withColumn("year",year(df['date'])).withColumn("month",month(df["date"]))
 
+# change based to 'level' to see level stats
+based = 'storage'
 
-print("years with highest average storage")
-df.groupBy("year").avg("storage").orderBy("avg(storage)", ascending=False).withColumn("avg(storage)",round(col('avg(storage)'),2)).show()
 
-# days with highest storage in each year - need to include the date also
-print("days with highest storage in each year")
-df.groupBy("year").max('storage').orderBy('max(storage)', ascending=False).show()
+print(f"years with highest average {based}")
+df.groupBy("year").avg(based).orderBy(f"avg({based})", ascending=False).withColumn(f"avg({based})",round(col(f'avg({based})'),2)).show()
 
-# days with lowest storage in each year - need to include the date
-print("days with lowest storage in each year")
-df.groupBy("year").min('storage').orderBy('min(storage)', ascending=False).show()
+print(f"days with highest {based} in each year")
+df1 = df.groupBy('year').max(based).withColumnRenamed('year','year1')
+df.join(df1,(df[based]==df1[f'max({based})']) & (df['year'] == df1['year1'])).groupBy('year').agg({'date':'min', based:'max'}).orderBy(f'max({based})',ascending=False).show()
+
+print(f"days with lowest {based} in each year")
+df1 = df.groupBy('year').min(based).withColumnRenamed('year','year1')
+df.join(df1,(df[based]==df1[f'min({based})']) & (df['year'] == df1['year1'])).groupBy('year').agg({'date':'min', based:'min'}).orderBy(f'min({based})',ascending=False).show()
 
 print("days with highest rainfall")
 df.select('date','rainfall').orderBy('rainfall',ascending=False).show(20)
@@ -30,7 +33,6 @@ df.select("date","inflow").orderBy("inflow",ascending=False).show(20)
 print("top 20 days with highest outflow")
 df.select("date","outflow").orderBy("outflow",ascending=False).show(20)
 
-# no of -1
 
 
 
